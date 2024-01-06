56dd626f38a9366e598adca8571dc241168ab5ca
Ahmed Khaled <1999ahmed127@gmail.com>
2024-01-06 23:50:45 +0200

    start point for HtDF module

---
+++ 2-htdf/htdf-readability.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname htdf-readability) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define WIDTH  100)
+(define HEIGHT 100)
+
+
+(define (half-tw x)
+  (+ x (/ (- WIDTH x) 2)))
