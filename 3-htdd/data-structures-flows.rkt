88822b2b844df0e05effa164cc1a84bdb34c4945
Ahmed Khaled <1999ahmed127@gmail.com>
2024-01-17 03:34:50 +0200

    Identifing The Inherent Structure of Information is a Key !

---
+++ 3-htdd/data-structures-flows.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname data-structures-flows) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Once we Identify the Inherent Structure of The Information,
+;; that gives us the structure of the data used to represent it,
+;; wich gives us
+;; - the structure of the template, which gives us the structure of the function
+;; - get Guidance about the Tests for the unctions 
